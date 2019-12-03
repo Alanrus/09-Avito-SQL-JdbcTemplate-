53333994dfab4f81c2355a7efc2206861e071bcd
Aleksandr Andreev <alan.tat@ya.ru>
2019-12-03 07:56:40 +0300

    added first version

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import model.House;
+import service.HouseService;
+import util.JdbcTemplate;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        HouseService houseService = new HouseService();
+        System.out.println("Получил данные из базы:");
+        System.out.println(houseService.getData());
+        System.out.println("Отсортировал данные по цене:");
+        System.out.println(houseService.sortByPrice());
+        System.out.println("Отсортировал по метро:");
+        System.out.println(houseService.sortByUnderground());
+        System.out.println("Поиск по цене:");
+        System.out.println(houseService.searchByPrice(1000000, 5000000));
+        System.out.println("Поиск по метро:");
+        System.out.println(houseService.searchByUnderground("Авиастрой"));
+
+    }
+}
